becd6573a9ddb1cb37780876f7c4796ec795dcb9
yung <yungi.chang@flowring.com>
2022-03-17 16:44:37 +0800

    初始化springboot

---
+++ src/main/java/com/yungtest/JavatestApplication.java
@@ -0,0 +1,13 @@
+package com.yungtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yungtest/JavatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yungtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
